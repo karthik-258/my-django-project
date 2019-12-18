439105a6fbdb441b4b42563e820ec02c80928d52
karthik-258 <mailtotkarthik@gmail.com>
2019-12-18 13:25:46 +0530

    updated dropdown

--- devops_blog/urls.py
+++ devops_blog/urls.py
@@ -7,5 +7,16 @@ urlpatterns = [
     path('blogform/',views.blogformview,name='blogform'),
     path('all_blogs/',views.all_blogsview,name='all_blogs'),
     path('thankyou/',views.thankyouview,name='thankyou'),
-    path('git/',views.gitview,name='git')
+    path('git/',views.gitview,name='git'),
+    path('jenkins/',views.jenkinsview,name='jenkins'),
+    path('ansible/',views.ansibleview,name='ansible'),
+    path('buildtools/',views.buildtoolsview,name='buildtools'),
+    path('cloud/',views.cloudview,name='cloud'),
+    path('datascience/',views.datascienceview,name='datascience'),
+    path('docker/',views.dockerview,name='docker'),
+    path('kubernates/',views.kubernatesview,name='kubernates'),
+    path('linux/',views.linuxview,name='linux'),
+    path('machinelearning/',views.machinelearningview,name='machinelearning'),
+    path('monitoringtools/',views.monitoringtoolsview,name='monitoringtools'),
+    path('python/',views.pythonview,name='python'),
 ]
--- devops_blog/views.py
+++ devops_blog/views.py
@@ -28,3 +28,58 @@ def gitview(request):
     my_list = blogmodel.objects.order_by('author_name')
     my_dict = {'form':my_list}
     return render(request,'devops_blog/git.html',context=my_dict)
+
+def jenkinsview(request):
+    my_list = blogmodel.objects.order_by('author_name')
+    my_dict = {'form':my_list}
+    return render(request,'devops_blog/jenkins.html',context=my_dict)
+
+def ansibleview(request):
+    my_list = blogmodel.objects.order_by('author_name')
+    my_dict = {'form':my_list}
+    return render(request,'devops_blog/ansible.html',context=my_dict)
+
+def buildtoolsview(request):
+    my_list = blogmodel.objects.order_by('author_name')
+    my_dict = {'form':my_list}
+    return render(request,'devops_blog/buildtools.html',context=my_dict)
+
+def cloudview(request):
+    my_list = blogmodel.objects.order_by('author_name')
+    my_dict = {'form':my_list}
+    return render(request,'devops_blog/cloud.html',context=my_dict)
+
+def datascienceview(request):
+    my_list = blogmodel.objects.order_by('author_name')
+    my_dict = {'form':my_list}
+    return render(request,'devops_blog/datascience.html',context=my_dict)
+
+def dockerview(request):
+    my_list = blogmodel.objects.order_by('author_name')
+    my_dict = {'form':my_list}
+    return render(request,'devops_blog/docker.html',context=my_dict)
+
+def kubernatesview(request):
+    my_list = blogmodel.objects.order_by('author_name')
+    my_dict = {'form':my_list}
+    return render(request,'devops_blog/kubernates.html',context=my_dict)
+
+def linuxview(request):
+    my_list = blogmodel.objects.order_by('author_name')
+    my_dict = {'form':my_list}
+    return render(request,'devops_blog/linux.html',context=my_dict)
+
+def machinelearningview(request):
+    my_list = blogmodel.objects.order_by('author_name')
+    my_dict = {'form':my_list}
+    return render(request,'devops_blog/machinelearning.html',context=my_dict)
+
+def monitoringtoolsview(request):
+    my_list = blogmodel.objects.order_by('author_name')
+    my_dict = {'form':my_list}
+    return render(request,'devops_blog/monitoringtools.html',context=my_dict)
+
+def pythonview(request):
+    my_list = blogmodel.objects.order_by('author_name')
+    my_dict = {'form':my_list}
+    return render(request,'devops_blog/python.html',context=my_dict)
